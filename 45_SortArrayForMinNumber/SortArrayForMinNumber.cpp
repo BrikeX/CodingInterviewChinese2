ac14d4d4210c3e8ad5bf2d13b170e13f4e2304df
BrikeX <gqxfk1997@126.com>
2021-06-21 17:07:26 +0800

    Modified according to C++11

--- 45_SortArrayForMinNumber/SortArrayForMinNumber.cpp
+++ 45_SortArrayForMinNumber/SortArrayForMinNumber.cpp
@@ -17,55 +17,83 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 接出的所有数字中最小的一个。例如输入数组{3, 32, 321}，则打印出这3个数
 // 字能排成的最小数字321323。
 
-#include "cstdio"
+#include <cstdio>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <iostream>
 
-int compare(const void* strNumber1, const void* strNumber2);
+// int compare(const void* strNumber1, const void* strNumber2);
 
-// int型整数用十进制表示最多只有10位
-const int g_MaxNumberLength = 10;
+// // int型整数用十进制表示最多只有10位
+// const int g_MaxNumberLength = 10;
  
-char* g_StrCombine1 = new char[g_MaxNumberLength * 2 + 1];
-char* g_StrCombine2 = new char[g_MaxNumberLength * 2 + 1];
+// char* g_StrCombine1 = new char[g_MaxNumberLength * 2 + 1];
+// char* g_StrCombine2 = new char[g_MaxNumberLength * 2 + 1];
  
+// void PrintMinNumber(const int* numbers, int length)
+// {
+//     if(numbers == nullptr || length <= 0)
+//         return;
+ 
+//     char** strNumbers = (char**)(new int[length]);
+//     for(int i = 0; i < length; ++i)
+//     {
+//         strNumbers[i] = new char[g_MaxNumberLength + 1];
+//         sprintf(strNumbers[i], "%d", numbers[i]);
+//     }
+ 
+//     qsort(strNumbers, length, sizeof(char*), compare);
+ 
+//     for(int i = 0; i < length; ++i)
+//         printf("%s", strNumbers[i]);
+//     printf("\n");
+ 
+//     for(int i = 0; i < length; ++i)
+//         delete[] strNumbers[i];
+//     delete[] strNumbers;
+// }
+ 
+// // 如果[strNumber1][strNumber2] > [strNumber2][strNumber1], 返回值大于0
+// // 如果[strNumber1][strNumber2] = [strNumber2][strNumber1], 返回值等于0
+// // 如果[strNumber1][strNumber2] < [strNumber2][strNumber1], 返回值小于0
+// int compare(const void* strNumber1, const void* strNumber2)
+// {
+//     // [strNumber1][strNumber2]
+//     strcpy(g_StrCombine1, *(const char**)strNumber1);
+//     strcat(g_StrCombine1, *(const char**)strNumber2);
+ 
+//     // [strNumber2][strNumber1]
+//     strcpy(g_StrCombine2, *(const char**)strNumber2);
+//     strcat(g_StrCombine2, *(const char**)strNumber1);
+ 
+//     return strcmp(g_StrCombine1, g_StrCombine2);
+// }
+
+bool strComparison(std::string& x, std::string& y)
+{
+    return x + y < y + x;
+}
+
 void PrintMinNumber(const int* numbers, int length)
 {
-    if(numbers == nullptr || length <= 0)
-        return;
- 
-    char** strNumbers = (char**)(new int[length]);
-    for(int i = 0; i < length; ++i)
+    std::string result;
+
+    std::vector<std::string> strs;
+
+    for (size_t i = 0; i < length; i++)
     {
-        strNumbers[i] = new char[g_MaxNumberLength + 1];
-        sprintf(strNumbers[i], "%d", numbers[i]);
+        strs.push_back(std::to_string(numbers[i]));
     }
- 
-    qsort(strNumbers, length, sizeof(char*), compare);
- 
-    for(int i = 0; i < length; ++i)
-        printf("%s", strNumbers[i]);
-    printf("\n");
- 
-    for(int i = 0; i < length; ++i)
-        delete[] strNumbers[i];
-    delete[] strNumbers;
-}
- 
-// 如果[strNumber1][strNumber2] > [strNumber2][strNumber1], 返回值大于0
-// 如果[strNumber1][strNumber2] = [strNumber2][strNumber1], 返回值等于0
-// 如果[strNumber1][strNumber2] < [strNumber2][strNumber1], 返回值小于0
-int compare(const void* strNumber1, const void* strNumber2)
-{
-    // [strNumber1][strNumber2]
-    strcpy(g_StrCombine1, *(const char**)strNumber1);
-    strcat(g_StrCombine1, *(const char**)strNumber2);
- 
-    // [strNumber2][strNumber1]
-    strcpy(g_StrCombine2, *(const char**)strNumber2);
-    strcat(g_StrCombine2, *(const char**)strNumber1);
- 
-    return strcmp(g_StrCombine1, g_StrCombine2);
+
+    std::sort(strs.begin(), strs.end(), strComparison);
+
+    for (size_t i = 0; i < length; i++)
+    {
+        result.append(strs[i]);
+    }
+    
+    std::cout << result << std::endl;
 }
 
 // ====================测试代码====================
