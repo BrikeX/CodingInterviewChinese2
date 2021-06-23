7a7694bdc5ed23c575e6a4bec948ca7acf855138
BrikeX <gqxfk1997@126.com>
2021-06-23 22:20:33 +0800

    Modified according to C++11

--- 66_ConstuctArray/ConstuctArray.cpp
+++ 66_ConstuctArray/ConstuctArray.cpp
@@ -18,6 +18,8 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 #include <cstdio>
 #include <vector>
+#include <cmath>
+#include <cfloat>
 
 using namespace std;
 
@@ -54,14 +56,14 @@ static bool EqualArrays(const vector<double>& input, const vector<double>& outpu
 
     for(int i = 0; i < length1; ++i)
     {
-        if(abs(input[i] - output[i]) > 0.0000001)
+        if(fabs(input[i] - output[i]) > DBL_EPSILON)
             return false;
     }
 
     return true;
 }
 
-static void test(char* testName, const vector<double>& input, vector<double>& output, const vector<double>& expected)
+static void test(const char* testName, const vector<double>& input, vector<double>& output, const vector<double>& expected)
 {
     printf("%s Begins: ", testName);
 
@@ -75,61 +77,51 @@ static void test(char* testName, const vector<double>& input, vector<double>& ou
 static void test1()
 {
     // 输入数组中没有0
-    double input[] = { 1, 2, 3, 4, 5 };
-    double output[] = { 0, 0, 0, 0, 0 };
-    double expected[] = { 120, 60, 40, 30, 24 };
+    vector<double> input = { 1, 2, 3, 4, 5 };
+    vector<double> output = { 0, 0, 0, 0, 0 };
+    vector<double> expected = { 120, 60, 40, 30, 24 };
 
-    test("Test1", vector<double>(input, input + sizeof(input) / sizeof(double)),
-        vector<double>(output, output + sizeof(output) / sizeof(double)),
-        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+    test("Test1", input, output, expected);
 }
 
 static void test2()
 {
     // 输入数组中有一个0
-    double input[] = { 1, 2, 0, 4, 5 };
-    double output[] = { 0, 0, 0, 0, 0 };
-    double expected[] = { 0, 0, 40, 0, 0 };
+    vector<double> input = { 1, 2, 0, 4, 5 };
+    vector<double> output = { 0, 0, 0, 0, 0 };
+    vector<double> expected = { 0, 0, 40, 0, 0 };
 
-    test("Test2", vector<double>(input, input + sizeof(input) / sizeof(double)),
-        vector<double>(output, output + sizeof(output) / sizeof(double)),
-        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+    test("Test2", input, output, expected);
 }
 
 static void test3()
 {
     // 输入数组中有两个0
-    double input[] = { 1, 2, 0, 4, 0 };
-    double output[] = { 0, 0, 0, 0, 0 };
-    double expected[] = { 0, 0, 0, 0, 0 };
+    vector<double> input = { 1, 2, 0, 4, 0 };
+    vector<double> output = { 0, 0, 0, 0, 0 };
+    vector<double> expected = { 0, 0, 0, 0, 0 };
 
-    test("Test3", vector<double>(input, input + sizeof(input) / sizeof(double)),
-        vector<double>(output, output + sizeof(output) / sizeof(double)),
-        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+    test("Test3", input, output, expected);
 }
 
 static void test4()
 {
     // 输入数组中有正、负数
-    double input[] = { 1, -2, 3, -4, 5 };
-    double output[] = { 0, 0, 0, 0, 0 };
-    double expected[] = { 120, -60, 40, -30, 24 };
+    vector<double> input = { 1, -2, 3, -4, 5 };
+    vector<double> output = { 0, 0, 0, 0, 0 };
+    vector<double> expected = { 120, -60, 40, -30, 24 };
 
-    test("Test4", vector<double>(input, input + sizeof(input) / sizeof(double)),
-        vector<double>(output, output + sizeof(output) / sizeof(double)),
-        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+    test("Test4", input, output, expected);
 }
 
 static void test5()
 {
     // 输入输入中只有两个数字
-    double input[] = { 1, -2 };
-    double output[] = { 0, 0 };
-    double expected[] = { -2, 1 };
+    vector<double> input = { 1, -2 };
+    vector<double> output = { 0, 0 };
+    vector<double> expected = { -2, 1 };
 
-    test("Test5", vector<double>(input, input + sizeof(input) / sizeof(double)),
-        vector<double>(output, output + sizeof(output) / sizeof(double)),
-        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+    test("Test5", input, output, expected);
 }
 
 int main(int argc, char* argv[])
